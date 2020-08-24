1a0718555e4c6f0e0dda95eb8c3f977178f9dd03
Sandy <saelric@gmail.com>
2020-08-24 17:59:02 -0500

    exercise-one

---
+++ object-exercise-one/src/ar/com/ada/online/second/objectexerciseone/Animal.java
@@ -0,0 +1,19 @@
+package ar.com.ada.online.second.objectexerciseone;
+
+public class Animal {
+    String breed;
+    String color;
+    String name;
+
+    void move(){
+        System.out.println("Estoy en move");
+    }
+
+    void sleep(){
+        System.out.println("Estoy en Sleep");
+    }
+
+    void eat(){
+        System.out.println("Estoy en eat");
+    }
+}
---
+++ object-exercise-one/src/ar/com/ada/online/second/objectexerciseone/Vehicle.java
@@ -0,0 +1,15 @@
+package ar.com.ada.online.second.objectexerciseone;
+
+public class Vehicle {
+    String brand;
+    Integer maxSpeed;
+    String color;
+
+    void moveOn(){
+        System.out.println("Estoy en move on");
+    }
+
+    void brake(){
+        System.out.println("Estoy en brake");
+    }
+}
