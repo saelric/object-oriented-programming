9605472b70426104ba8bb398031ba68034a09242
Sandy <saelric@gmail.com>
2020-08-24 22:22:58 -0500

    creación clase Item

---
+++ object-exercise-two/src/ar/com/ada/online/second/objectexercisetwo/Item.java
@@ -0,0 +1,15 @@
+package ar.com.ada.online.second.objectexercisetwo;
+
+import java.util.Date;
+
+public class Item {
+    String name;
+    Double price;
+    Double tax;
+    Date expirationDate;
+
+    Double getTotalToPay(){
+        Double result = 4.3;
+        return (result);
+    }
+}
